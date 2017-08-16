21204cea73a4aeaaacfeb5c27f182607accb4afa
kobra8 <jerzydrobot@gmail.com>
2017-08-16 19:51:40 +0200

    Merge

--- KomponentyAng2/lib/app.js
+++ KomponentyAng2/lib/app.js
@@ -1,11 +1,44 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import {} from './project/project-service/project-service';
+import { ProjectService } from './project/project-service/project-service';
 import template from './app.html !text';
+
+class LinkConventer {
+  static getIdFromLink(link) {
+    return link.slice(1);
+  }
+  static getItemmodelFromproject(project) {
+    return project ? {
+      title: project.title,
+      link: `#$(project._id)`
+    } : {
+      title: '',
+      link: '#'
+    };
+  }
+}
 
 @Component({
   selector: 'ngc-app',
   template,
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers: [ProjectService]
 })
 
-export class App {}
+export class App {
+  constructor(@Inject(ProjectService) projectService) {
+    this.projectService = projectService;
+    this.projects = [];
+
+    this.projectsSubscription = projectService.change
+    .subscribe((projects) => {
+      this.projects = projects;
+
+      this.projectNavigationItems = this.projects
+      .filter((projects) => !project.deleted)
+      .map((project) => LinkConventer.getItemmodelFromproject(project));
+      if(this.selectedProject) {
+        this.selectedProject = this.projects.find((project) => project._id === this.selectedProject._id);
+      }
+    })
+  }
+}
